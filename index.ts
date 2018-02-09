fb610e068a9cd8386ffe3653bc985b9786c5cf7f
Yan Yankowski <yan.yankowski@zalando.de>
2018-02-09 20:26:47 +0100

    reusability added

--- index.ts
+++ index.ts
@@ -30,20 +30,58 @@ namespace Simply {
         return new Date(sDt.year, sDt.month, sDt.day, sDt.hour, sDt.minute, sDt.second, sDt.millisecond);
     }
 
-    const to = (obj: {[key: string]: string}) => (sDt: SimplyDate) => 
-        Object.assign({...sDt}, obj);
+    const incBy = (n1: number, n2: number) => n1 + n2; 
 
-    export const add = (value: number) => ({
-        month: () => ({
-            to: ({year, month, day, hour, minute, second, millisecond}: SimplyDate) => ({
-                year,
-                month : month + 1,
-                day,
-                hour,
-                minute,
-                second,
-                millisecond,
-            })
+    const decBy = (n1: number, n2: number) => n1 - n2;
+
+    /**
+     * Adds a value to the SimplyDate
+     * @param obj {month: 4}
+     */
+    const _to = (action: Function) => 
+        ({year, month, day, hour, minute, second, millisecond}: SimplyDate) => 
+        (sDt: SimplyDate): SimplyDate => 
+        Object.assign({...sDt}, {
+            year: year ? action(year, sDt.year) : sDt.year,
+            month: month ? action(month, sDt.month) : sDt.month,
+            day: day ? action(day, sDt.day) : sDt.day,
+            hour: hour ? action(hour, sDt.hour) : sDt.hour,
+            minute : minute ? action(minute, sDt.minute) : sDt.minute,
+            second : second ? action(second, sDt.second) : sDt.second,
+            millisecond : millisecond ? action(millisecond, sDt.millisecond) : sDt.millisecond
+        });
+
+    const _toInc = _to(incBy);
+    const _toDec = _to(decBy);
+    const _transform = (preposition: string, action: (p: SimplyDate) => (p1: SimplyDate) => SimplyDate) => (value: number) => ({
+        years: () => ({
+            [preposition]: action({year: value} as SimplyDate)
+        }),
+        months: () => ({
+            [preposition]: action({month: value} as SimplyDate)
+        }),
+        days: () => ({
+            [preposition]: action({day: value} as SimplyDate)
+        }),
+        hours: () => ({
+            [preposition]: action({hour: value} as SimplyDate)
+        }),
+        minutes: () => ({
+            [preposition]: action({minute: value} as SimplyDate)
+        }),
+        seconds: () => ({
+            [preposition]: action({second: value} as SimplyDate)
+        }),
+        milliseconds: () => ({
+            [preposition]: action({millisecond: value} as SimplyDate)
         })
     })
+
+    /**
+     * Add 
+     * @param value 
+     */
+    export const add =  _transform("to", _toInc);
+    export const subtract = _transform("from", _toDec);
+
 }
