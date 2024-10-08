3bdbf7039c22f17583381c4c3d653c166e069d68
Yan Yankowski <1254242+yyankowski@users.noreply.github.com>
2024-10-08 18:25:40 +0200

    chore: fix negative value addition/subtraction issue

--- test/milliseconds.spec.ts
+++ test/milliseconds.spec.ts
@@ -1,32 +1,29 @@
-import { Simply, SimplyDate } from '../src';
+import { Simply, SimplyDate } from "../src";
 
-describe('milliseconds.spec.ts', () => {
-	let sDt: SimplyDate;
+describe("milliseconds.spec.ts", () => {
+  let sDt: SimplyDate;
 
-	describe('1.0 subtraction ', () => {
+  describe("1.0 subtraction ", () => {
+    it("1.1 should correctly subtract milliseconds", () => {
+      sDt = Simply.from.date(new Date("2017-03-01T03:24:00"));
+      expect(Simply.subtract(500).milliseconds.from(sDt).millisecond).toBe(500);
+      expect(Simply.subtract(500).milliseconds.from(sDt).second).toBe(59);
+      expect(Simply.subtract(500).milliseconds.from(sDt).minute).toBe(23);
+    });
+  });
 
-		it('1.1 should correctly subtract milliseconds', () => {
-			sDt = Simply.from.date(new Date('2017-03-01T03:24:00'));
-			expect(Simply.subtract(500).milliseconds.from(sDt).millisecond).toBe(500);
-			expect(Simply.subtract(500).milliseconds.from(sDt).second).toBe(59);
-			expect(Simply.subtract(500).milliseconds.from(sDt).minute).toBe(23);
-		});
+  describe("2.0 addition ", () => {
+    it("2.1 should correctly add milliseconds", () => {
+      sDt = Simply.from.date(new Date("2017-03-01T03:24:00"));
+      expect(Simply.add(500).milliseconds.to(sDt).millisecond).toBe(500);
+      expect(Simply.add(500).milliseconds.to(sDt).second).toBe(0);
+    });
 
-	});
-
-	describe('2.0 addition ', () => {
-
-		it('2.1 should correctly add milliseconds', () => {
-			sDt = Simply.from.date(new Date('2017-03-01T03:24:00'));
-			expect(Simply.add(500).milliseconds.to(sDt).millisecond).toBe(500);
-			expect(Simply.add(500).milliseconds.to(sDt).second).toBe(0);
-		});
-
-		it('2.2 should correctly add milliseconds', () => {
-			sDt = Simply.from.date(new Date('2017-03-01T03:24:59'));
-			expect(Simply.add(1000).milliseconds.to(sDt).millisecond).toBe(0);
-			expect(Simply.add(1000).milliseconds.to(sDt).second).toBe(0);
-			expect(Simply.add(1000).milliseconds.to(sDt).minute).toBe(25);
-		});
-	});
+    it("2.2 should correctly add milliseconds", () => {
+      sDt = Simply.from.date(new Date("2017-03-01T03:24:59"));
+      expect(Simply.add(1000).milliseconds.to(sDt).millisecond).toBe(0);
+      expect(Simply.add(1000).milliseconds.to(sDt).second).toBe(0);
+      expect(Simply.add(1000).milliseconds.to(sDt).minute).toBe(25);
+    });
+  });
 });
--- test/minutes.spec.ts
+++ test/minutes.spec.ts
@@ -1,35 +1,35 @@
-import { Simply, SimplyDate } from '../src';
+import { Simply, SimplyDate } from "../src";
 
-describe('minutes.spec.ts', () => {
-	let sDt: SimplyDate;
+describe("minutes.spec.ts", () => {
+  let sDt: SimplyDate;
 
-	describe('1.0 subtraction ', () => {
-		beforeEach(() => {
-			sDt = Simply.from.date(new Date('2017-03-01T03:24:00'));
-		});
+  describe("1.0 subtraction ", () => {
+    beforeEach(() => {
+      sDt = Simply.from.date(new Date("2017-03-01T03:24:00"));
+    });
 
-		it('1.1 correctly subtract minutes', () => {
-			expect(Simply.subtract(24).minutes.from(sDt).hour).toBe(3);
-			expect(Simply.subtract(24).minutes.from(sDt).minute).toBe(0);
-		});
+    it("1.1 correctly subtract minutes", () => {
+      expect(Simply.subtract(24).minutes.from(sDt).hour).toBe(3);
+      expect(Simply.subtract(24).minutes.from(sDt).minute).toBe(0);
+    });
 
-		it('1.2 correctly subtract minutes', () => {
-			expect(Simply.subtract(25).minutes.from(sDt).minute).toBe(59);
-		});
-	});
+    it("1.2 correctly subtract minutes", () => {
+      expect(Simply.subtract(25).minutes.from(sDt).minute).toBe(59);
+    });
+  });
 
-	describe('2.0 addition ', () => {
-		beforeEach(() => {
-			sDt = Simply.from.date(new Date('2017-03-01T03:24:00'));
-		});
+  describe("2.0 addition ", () => {
+    beforeEach(() => {
+      sDt = Simply.from.date(new Date("2017-03-01T03:24:00"));
+    });
 
-		it('2.1 correctly add minutes', () => {
-			expect(Simply.add(24).minutes.to(sDt).hour).toBe(3);
-			expect(Simply.add(36).minutes.to(sDt).minute).toBe(0);
-		});
+    it("2.1 correctly add minutes", () => {
+      expect(Simply.add(24).minutes.to(sDt).hour).toBe(3);
+      expect(Simply.add(36).minutes.to(sDt).minute).toBe(0);
+    });
 
-		it('2.2 correctly add minutes', () => {
-			expect(Simply.add(36).minutes.to(sDt).hour).toBe(4);
-		});
-	});
+    it("2.2 correctly add minutes", () => {
+      expect(Simply.add(36).minutes.to(sDt).hour).toBe(4);
+    });
+  });
 });
